0bb167bec4777d27e3ef05e189194eb504117dd6
Emma Tenenbaum <emmatenenbaum@mail.adelphi.edu>
2026-05-01 10:03:55 -0400

    hi

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# Assignment8
-CSC 175 Group Project
+# Turtle Shape Drawing Lab 8
+## Stack Snakes
+
+## Overview
+Java project that uses the Turtle class to draw shapes using object-oriented programming
+
+## Features
+-Abstract `Shape` class that all shapes share
+-Concrete shape classes
+   1. *Rectangle* class
+   2. *Circle* class
+   3. *Triangle* class
+-Turtle Library used for drawing shapes
+-Customizable qualities
+   1. *Color*
+   2. *Size*
+   3. *Position*
+-Inheritance and Method overriding
+-`App.java` main method
+
+## Classes
+
+### `Shape` (Abstract Class)
+-Stores shared properties like color, size, and position
+-Implements the Turtle library 
+-Defines abstract method `draw()`
+
+### `Rectangle`
+-Extends `Shape`
+-Adds width
+-Overrides `draw()`, `toString()`, and `equals()`
+-Uses Turtle to draw rectangle
+
+### `Circle`
+-Extends `Shape`
+
+
+### `Triangle`
+-Extands `Shape`
+
+
+## How to Run
+1. Compile all Java files
+2. Run the program
+3. Follow any prompts given
+
+## Screenshots
+
+
+## Libraries
+-`Turtle.java` by NICHOLAS SEWARD
+
+## Authors
+### Scott Benes
+    - `Shape` class
+    - `Circle` class
+### Emma Tenenbaum
+    - `Rectangle` class
+    - **README.md**
+### Jarren Mcknight
+    - `Triangle` class
+    - `App.java` main class
---
+++ Rectangle.java
@@ -0,0 +1,125 @@
+/** Represents a rectangle that is drawm using the turtle class
+ * Has a width from Rectangle class and height inherited from Shape class (size)
+ * 
+ */
+
+
+public class Rectangle extends Shape{
+
+    private int width;
+    
+    /**
+     * Creates a rectanlge with a default color and height
+     * sets the width to 100
+     */
+    public Rectangle() {
+        super();
+        this.width = 100;
+     
+    }
+    /**
+     * Creates a rectangle with custom color, width, and height
+     * @param color the color of the rectangle
+     * @param width the width of the rectangle > 0
+     * @param height the height of the rectangle from shape class
+     */
+    public Rectangle(String color, int width, int height){
+        super(color, height);
+        
+        if(width <= 0){
+            throw new IllegalArgumentException("Width must be greater than 0");
+        }
+        this.width = width;
+    }
+    
+
+    /**
+     * Draws the rectangle using Turtle class
+     */
+    @Override
+    public void draw() {
+        colorShape();
+
+        for (int i = 0; i<2; i++) {
+            turtle.forward(width);
+            turtle.right(90);
+            turtle.forward(getSize());
+            turtle.right(90);
+        }
+
+    
+    }
+    
+
+    /**
+     * Compares the rectangle to another object
+     * @param object the object to compare
+     * @return true if they are equal, false otherwise
+     */
+    @Override
+    public boolean equals(Object object) {
+        if(this == object){
+            return true;
+        }
+        if (!(object instanceof Rectangle)){
+            return false;
+        }
+
+        Rectangle other = (Rectangle) object;
+
+        return this.getColor().equals(other.getColor()) &&
+        this.getSize() == other.getSize() &&
+        this.width == other.width;
+
+    }
+    /**
+     * Returns a string representing the rectangle
+     * @return string describing the rectangle
+     */
+    @Override
+    public String toString(){
+        return "Rectangle: color = " + getColor() +
+               ", height = " + getSize() +
+               ", width = " + width;   
+            }
+    
+    /**
+     * Gets the width of the rectangle
+     * @return width an integer
+     */
+    public int getWidth(){
+        return width;
+    }  
+    
+    /**
+     * Gets the height from the shape class (size)
+     * @return hieght an integer
+     */
+    public int getHeight() {
+        return getSize();
+    }
+
+    /**Sets the width of the rectangle
+     * @param width an integer > 0
+     */
+    public void setWidth(int width) {
+        if(width <= 0){
+            throw new IllegalArgumentException("Width must be greater than 0");
+        }
+        this.width = width;
+    }
+
+    /**
+     * Sets the height of the rectangle
+     * Updates the size in shape class
+     * @param height ann integer > 0
+     */
+    public void setHeight(int height){
+        if(height <= 0){
+            throw new IllegalArgumentException("Height must be greater than 0");
+        }
+        setSize(height);
+    }
+
+
+}
